2eb2d639f625ee5e925cce0d70d5c9f059788e52
wighthood <zefires@gmail.com>
2024-01-08 16:03:01 +0100

    Initial commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody RB;
+    public float MoveSpeed = 1.0f;
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        
+    }
+}
