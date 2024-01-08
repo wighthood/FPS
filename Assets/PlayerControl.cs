e8457c41bd4364f38207f7592667c3faf135c331
wighthood <zefires@gmail.com>
2024-01-08 16:38:53 +0100

    horizontal movement
    implemented horizontal movement

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    public Rigidbody RB;
-    public float MoveSpeed = 1.0f;
+    PlayerInput playerInput;
+    InputAction moveAction;
+    public float speed = 5.0f;
 
-    public void Move(InputAction.CallbackContext context)
+    private void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+    }
+
+    private void Update()
+    {
+        movePlayer();
+    }
+    void movePlayer()
+    {
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, direction.y)* speed *Time.deltaTime;
     }
 }
